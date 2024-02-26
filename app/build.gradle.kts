ad2f6f051eac92f9734e5eebb708235d6b72ead6
Anderpascal <50362627+Anderpascal@users.noreply.github.com>
2024-02-26 11:28:26 +0100

    Final commit 2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -67,6 +67,7 @@ android {
 }
 
 dependencies {
+
     implementation(platform("androidx.compose:compose-bom:2023.06.00"))
     implementation("androidx.activity:activity-compose:1.7.2")
     implementation("androidx.compose.material:material-icons-extended")
